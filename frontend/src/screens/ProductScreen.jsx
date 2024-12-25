c19fd3862ac39c93f8e2941b9b565314f625baf2
Kamal <kamallogin8@gmail.com>
2024-12-25 20:22:34 +0530

    fetch product from backend in frontend using axios

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,12 +2,24 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import {Row, Col, Image, ListGroup, Card, Button} from 'react-bootstrap';
-import products from '../products'
+// import products from '../products'
 import Rating from '../components/Rating'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const ProductScreen = () => {
     const { id: productId } = useParams()
-    const product = products.find((p) => p._id === productId);
+
+    const [product, setProduct] = useState({})
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [productId]);
+
+    // const product = products.find((p) => p._id === productId);
     // console.log(product)   ;
     return (
         <>
