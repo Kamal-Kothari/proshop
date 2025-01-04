65c080828b8d23bd244a0438a5605cfa416815fc
Kamal <kamallogin8@gmail.com>
2025-01-04 23:00:52 +0530

    logout clear cookie remove user detail from req.user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -44,7 +44,13 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/logout
 // @access  private
 const logoutUser = asyncHandler(async (req, res) => {
-    res.send('logout user');
+    // res.send('logout user');
+    req.user = null;
+    res.clearCookie('jwt',  {
+        expires: new Date(0),
+        httpOnly: true
+    });
+    res.status(200).json({message: 'logged out'});
   });
 
 // @desc    Get user profile
