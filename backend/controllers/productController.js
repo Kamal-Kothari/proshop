d97cdffc27609b6869a423175ae324a2957e7bed
Kamal <kamallogin8@gmail.com>
2025-02-03 12:02:17 +0530

    add review for a product backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -76,4 +76,43 @@ const deleteProductById = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Product removed successfully' });
 });
 
-export { getAllProducts, getProductById, createProduct, updateProduct, deleteProductById };
+
+// @desc    create  a review for a product
+// @route   post /api/products/:id/reviews
+// @access  Private
+const createReview = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const product = await Product.findById(id);
+    if (!product) {
+        res.status(404);
+        throw new Error('Product not found');
+    }
+
+    const { rating, comment } = req.body;
+    const alreadyReviewed = product.reviews.find(
+        (review) => review.user.toString() === req.user._id.toString()
+    );
+    if (alreadyReviewed) {
+        res.status(400);
+        throw new Error('Product already reviewed');
+    }
+
+    const review = {
+        name: req.user.name,
+        rating: Number(rating),
+        comment,
+        user: req.user._id,
+    };
+
+    product.reviews.push(review);
+    product.numReviews = product.reviews.length;
+    product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+
+    await product.save();
+
+    res.status(201).json({ message: 'Review added' });
+
+});
+
+export { getAllProducts, getProductById, createProduct, updateProduct, deleteProductById, createReview };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 // import asyncHandler from '../middleware/asyncHandler.js';
 // import Product from '../models/productModel.js';
 // import products from '../data/products.js';
-import { getAllProducts, getProductById, createProduct, updateProduct, deleteProductById } from '../controllers/productController.js';
+import { getAllProducts, getProductById, createProduct, updateProduct, deleteProductById, createReview } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -12,5 +12,6 @@ router.route('/').get(getAllProducts).post(protect, admin, createProduct);
 router.get('/:id', getProductById);
 router.delete('/:id', protect, admin, deleteProductById);
 router.put('/:id/edit', protect, admin, updateProduct);
+router.post('/:id/reviews', protect, createReview);
 
 export default router;
